3f11c17750777c103df4620e969f255f217a9d57
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-19 13:34:16 -0200

    Support cors requests

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+    next();
+});
+
 app.use(bodyParser.json());
 
 require('./routes/user')(app)
