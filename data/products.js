7a4f688a043c33fee9a0e57e3562ab7ebbef487f
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-20 20:23:04 -0200

    Change property names

--- data/products.js
+++ data/products.js
@@ -1,25 +1,25 @@
 module.exports = [{
     userId: 1,
-    productName: 'Refrigerador Brastemp Frost Free BRM54HB 400 Litros 2 Portas Branco',
-    productDescription: 'Design e praticidade vão invadir a sua cozinha! A Geladeira Brastemp Frost Free Duplex BRM54 tem 400 litros de capacidade e conta com o exclusivo compartimento Freeze Control, que conserva carnes por até 5 dias sem congelar*, além de deixar tanto bebidas quanto frios e laticínios na temperatura ideal. Pelo Painel Eletrônico você acessa o Controle de Temperatura e os Modos Especiais: Compras, Festa e Férias. Ela tem, ainda, Twist Ice para desenformar e armazenar gelo facilmente.',
-    productImage: 'https://static.wmobjects.com.br/imgres/arquivos/ids/12389720-344-344/refrigerador-brastemp-frost-free-brm54hb-400-litros-2-portas-branco.jpg',
+    name: 'Refrigerador Brastemp Frost Free BRM54HB 400 Litros 2 Portas Branco',
+    description: 'Design e praticidade vão invadir a sua cozinha! A Geladeira Brastemp Frost Free Duplex BRM54 tem 400 litros de capacidade e conta com o exclusivo compartimento Freeze Control, que conserva carnes por até 5 dias sem congelar*, além de deixar tanto bebidas quanto frios e laticínios na temperatura ideal. Pelo Painel Eletrônico você acessa o Controle de Temperatura e os Modos Especiais: Compras, Festa e Férias. Ela tem, ainda, Twist Ice para desenformar e armazenar gelo facilmente.',
+    image: 'https://static.wmobjects.com.br/imgres/arquivos/ids/12389720-344-344/refrigerador-brastemp-frost-free-brm54hb-400-litros-2-portas-branco.jpg',
     wishlist: true
 }, {
     userId: 1,
-    productName: 'Micro-ondas 31 Litros Electrolux Blue Touch MEC41 Inox Com Grill e Porta Espelhada',
-    productDescription: 'Com acabamento total em aço inox, o Micro-ondas Blue Touch, da Electrolux, é ideal para deixar o seu dia a dia prático e moderno. Possui programas das 3 receitas mais utilizadas pelo consumidor, além de grill para doura os alimentos, dando um toque a mais nas receitas.',
-    productImage: 'https://static.wmobjects.com.br/imgres/arquivos/ids/13394600-344-344/micro-ondas-31-litros-electrolux-blue-touch-mec41-inox-com-grill-e-porta-espelhada.jpg',
+    name: 'Micro-ondas 31 Litros Electrolux Blue Touch MEC41 Inox Com Grill e Porta Espelhada',
+    description: 'Com acabamento total em aço inox, o Micro-ondas Blue Touch, da Electrolux, é ideal para deixar o seu dia a dia prático e moderno. Possui programas das 3 receitas mais utilizadas pelo consumidor, além de grill para doura os alimentos, dando um toque a mais nas receitas.',
+    image: 'https://static.wmobjects.com.br/imgres/arquivos/ids/13394600-344-344/micro-ondas-31-litros-electrolux-blue-touch-mec41-inox-com-grill-e-porta-espelhada.jpg',
     wishlist: true
 }, {
     userId: 1,
-    productName: 'Secador de Cabelos Britânia Easy Travel Preto 1000W Bivolt',
-    productDescription: 'O Secador de Cabelo da Britânia Easy Travel, é compacto e tem potência de 1000W. Ele conta com 2 velocidades e 2 opções de temperatura permitindo combiná-las. Vem com cabo dobrável de 1,45 metros e bico concentrador.',
-    productImage: 'https://static.wmobjects.com.br/imgres/arquivos/ids/11802031-344-344/secador-de-cabelos-britania-easy-travel-preto-1000w-bivolt.jpg',
+    name: 'Secador de Cabelos Britânia Easy Travel Preto 1000W Bivolt',
+    description: 'O Secador de Cabelo da Britânia Easy Travel, é compacto e tem potência de 1000W. Ele conta com 2 velocidades e 2 opções de temperatura permitindo combiná-las. Vem com cabo dobrável de 1,45 metros e bico concentrador.',
+    image: 'https://static.wmobjects.com.br/imgres/arquivos/ids/11802031-344-344/secador-de-cabelos-britania-easy-travel-preto-1000w-bivolt.jpg',
     ordered: true
 }, {
     userId: 1,
-    productName: 'Climatizador de Ar Cadence Frio CLI302 Branco',
-    productDescription: 'O Climatizador de Ar Frio CLI302 Branco, da marca Cadence, oferece 3 velocidades de ventilação, utiliza a umidade do próprio ar para abaixar a temperatura dos cômodos e circula o ar no ambiente. Com painel superior, a temperatura pode ser controlada entre as funções esfriar e oscilar.',
-    productImage: 'https://static.wmobjects.com.br/imgres/arquivos/ids/12986093-344-344/climatizador-de-ar-cadence-frio-cli302-branco.jpg',
+    name: 'Climatizador de Ar Cadence Frio CLI302 Branco',
+    description: 'O Climatizador de Ar Frio CLI302 Branco, da marca Cadence, oferece 3 velocidades de ventilação, utiliza a umidade do próprio ar para abaixar a temperatura dos cômodos e circula o ar no ambiente. Com painel superior, a temperatura pode ser controlada entre as funções esfriar e oscilar.',
+    image: 'https://static.wmobjects.com.br/imgres/arquivos/ids/12986093-344-344/climatizador-de-ar-cadence-frio-cli302-branco.jpg',
     ordered: true
 }]
