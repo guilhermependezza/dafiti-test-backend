378f15f32d0284df39af535b39ffa33c5baa632c
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-22 17:06:33 -0200

    Change address info

--- routes/address-list.js
+++ routes/address-list.js
@@ -1,11 +1,16 @@
 let addressList = [{
     userId: 1,
     street: 'Acacia Avenue',
-    number: '22'
+    number: '22',
+    city: 'Catanduva',
+    state: 'SP'
+
 }, {
     userId: 1,
     street: 'Frei Inácio da Conceição',
-    number: '629'
+    number: '629',
+    city: 'São Paulo',
+    state: 'SP'
 }]
 
 module.exports = app => {
